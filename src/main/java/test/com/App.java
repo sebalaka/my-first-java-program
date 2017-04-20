87e4e5e8838b4aad153ca3b09afe743eb65e760a
sebalaka <sebalaka@gmail.com>
2017-04-20 18:18:31 +0200

    my first commit

---
+++ src/main/java/test/com/App.java
@@ -0,0 +1,16 @@
+package test.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
